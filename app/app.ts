a499f07796a1a511f408dd73ef52aa4cf2cdf82f
Kamil Gałuszka <galuszkak@gmail.com>
2016-07-07 20:30:32 +0200

    initial app

---
+++ app/app.ts
@@ -0,0 +1,33 @@
+import {Component, provide, ExceptionHandler} from '@angular/core';
+import {Platform, ionicBootstrap} from 'ionic-angular';
+import {StatusBar} from 'ionic-native';
+import {TabsPage} from './pages/tabs/tabs';
+import Raven from 'raven-js';
+
+Raven.config('__PUBLIC_DNS__').install();
+
+class RavenExceptionHandler {
+  call(err:any) {
+    Raven.captureException(err.originalException);
+  }
+}
+
+@Component({
+  template: '<ion-nav [root]="rootPage"></ion-nav>'
+})
+export class MyApp {
+
+  private rootPage:any;
+
+  constructor(private platform:Platform) {
+    this.rootPage = TabsPage;
+
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+    });
+  }
+}
+
+ionicBootstrap(MyApp, [provide(ExceptionHandler, {useClass: RavenExceptionHandler})]);
